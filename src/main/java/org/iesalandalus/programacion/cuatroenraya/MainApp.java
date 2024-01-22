b037d1cea6aa85e2dc16d4a69457388674cb67f8
Juanmi <juanmi-64@hotmail.com>
2024-01-22 01:37:00 +0100

    22/01/24

--- src/main/java/org/iesalandalus/programacion/cuatroenraya/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/MainApp.java
@@ -1,10 +1,17 @@
 package org.iesalandalus.programacion.cuatroenraya;
 
+import org.iesalandalus.programacion.cuatroenraya.modelo.CuatroEnRaya;
+import org.iesalandalus.programacion.cuatroenraya.modelo.Ficha;
+import org.iesalandalus.programacion.cuatroenraya.vista.Consola;
+
+import javax.naming.OperationNotSupportedException;
+
 public class MainApp {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws OperationNotSupportedException {
 
-
+		CuatroEnRaya cuatroEnRaya = new CuatroEnRaya(Consola.leerJugador(), Consola.leerJugador(Ficha.VERDE));
+		cuatroEnRaya.jugar();
 	}
 	
 }
